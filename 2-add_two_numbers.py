dea639dde78233cd16e548128991787439216f6d
Sreeraj9764 <cnsreeraj97@gmail.com>
2024-07-23 08:31:11 +0530

    two sum

---
+++ 2-add_two_numbers.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+class ListNode(object):
+    def __init__(self, val:int =0, next:ListNode =None):
+        self.val = val
+        self.next = next
+    def __str__(self):
+        if self.next is None:
+            return f"{self.val}"
+        else:
+            return f"{self.val} --> {self.next}"
+
+class Solution(object):
+    def addTwoNumbers(self, l1:ListNode, l2:ListNode) -> int:
