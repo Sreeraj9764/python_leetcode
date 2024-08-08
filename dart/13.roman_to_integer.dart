fc0c4b14cbabd1de5a1bbe492792c470b662a006
sreeraj4 <sreeraj@techxers.com>
2024-08-08 18:20:14 +0530

    updates

--- dart/13.roman_to_integer.dart
+++ dart/13.roman_to_integer.dart
@@ -7,19 +7,35 @@ Map<String, int> romanValues = {
   "D": 500,
   "M": 1000
 };
+//1
+// int romanToInt(String s) {
+//   int total = 0;
+//   String lastAdded = "";
+//   for (int i = 0; i < s.length; i++) {
+//     int? lastAddedValue = romanValues[lastAdded];
+//     int currentValue = romanValues[s[i]]!;
+//     if (lastAdded.isNotEmpty && (lastAddedValue! < currentValue)) {
+//       total += currentValue;
+//       total -= (lastAddedValue * 2);
+//     } else {
+//       total += currentValue;
+//     }
+//     lastAdded = s[i];
+//   }
+//   return total;
+// }
+
+//2
+
 int romanToInt(String s) {
   int total = 0;
-  String lastAdded = "";
   for (int i = 0; i < s.length; i++) {
-    int? lastAddedValue = romanValues[lastAdded];
     int currentValue = romanValues[s[i]]!;
-    if (lastAdded.isNotEmpty && (lastAddedValue! < currentValue)) {
-      total += currentValue;
-      total -= (lastAddedValue * 2);
+    if (i != s.length - 1 && currentValue < romanValues[s[i + 1]]!) {
+      total -= currentValue;
     } else {
       total += currentValue;
     }
-    lastAdded = s[i];
   }
   return total;
 }
